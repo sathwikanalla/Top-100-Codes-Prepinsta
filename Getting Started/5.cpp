dbe573348659f3af87c03576b9498001d7b0c9f4
sathwikanalla <sathwikanalla29@gmail.com>
2023-10-27 21:42:28 +0530

    Getting started with 100 codes

---
+++ Getting Started/5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int num1 = 5;
+    int num2 = 10;
+    int sum = 0;
+    for (int i = num1; i <= num2; i++)
+        sum = sum + i;
+    cout << sum; 
+    return 0;
+}
